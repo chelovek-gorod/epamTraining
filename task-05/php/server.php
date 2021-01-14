63354bd8d24326ab4e0bf00aa0faf13efe60971f
Marsel <47145234+chelovek-gorod@users.noreply.github.com>
2021-01-14 15:02:31 +0300

    Add files via upload

---
+++ task-05/php/server.php
@@ -0,0 +1,16 @@
+<?php
+
+$str_json = file_get_contents('php://input');
+$response = json_decode($str_json, true);
+
+$to = "chelovek-gorod@yandex.by";
+$theme = "Calc aarays"; 
+$message = $str_json; 
+$headers  = "MIME-Version: 1.0" . "\r\n";
+$headers .= "From: calc-array@bit-studio.by" . "\r\n";
+$headers .= "Content-type: text/html; charset=utf-8" . "\r\n";
+$send_ok = mail($to, $theme, $message, $headers);
+
+echo $str_json;
+
+?>
